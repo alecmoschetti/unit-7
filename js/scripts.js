9688f697969fef9422917da742ccd1489ecbb1fe
Alec Moschetti <49801141+alecmoschetti@users.noreply.github.com>
2020-07-06 16:21:19 -0700

    started putting first chart.js file in

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+let trafficChart = document.getElementById('traffic-chart').getContext('2d');
+
+Chart.defaults.global.defaultFontSize = 14;
+let chart = new Chart(trafficChart, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+        maintainAspectRatio: false, 
+    }
+});
